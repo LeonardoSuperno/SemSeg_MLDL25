cd734f8add72365d7b315d66a0c5772c4b9bb453
giovannistina <giovanni.stina@gmail.com>
2025-04-24 11:04:34 +0200

    Update cityscapes.py

--- datasets/cityscapes.py
+++ datasets/cityscapes.py
@@ -1,47 +1,59 @@
 from torch.utils.data import Dataset
+import torch
 from PIL import Image
 import os
 import numpy as np
+from typing import Optional, Tuple
+from albumentations import Compose
 
 
 class CityScapes(Dataset):
-    def __init__(self, root_dir, split='train', transform=None):
-        super(CityScapes, self).__init__()
-        self.root_dir = root_dir
-        self.split = split
+    """
+    Custom Dataset for the Cityscapes dataset.
+    Loads images and their corresponding labelTrainIds masks.
+    """
+    def __init__(self, 
+                 root_dir: str, 
+                 split: str = 'train', 
+                 transform: Optional[Compose] = None):
+        """
+        Args:
+            root_dir (str): Root directory of Cityscapes dataset.
+            split (str): Dataset split, one of ['train', 'val', 'test'].
+            transform (Optional[Compose]): Albumentations transform to apply.
+        """
+        super().__init__()
         self.transform = transform
+        self.samples = []
 
-        self.images_dir = os.path.join(root_dir, "leftImg8bit", split)
-        self.labels_dir = os.path.join(root_dir, "gtFine", split)
+        img_dir = os.path.join(root_dir, "leftImg8bit", split)
+        lbl_dir = os.path.join(root_dir, "gtFine", split)
 
-        self.image_paths = []
-        self.label_paths = []
+        for city in os.listdir(img_dir):
+            img_city_path = os.path.join(img_dir, city)
+            lbl_city_path = os.path.join(lbl_dir, city)
+            for fname in os.listdir(img_city_path):
+                if fname.endswith("_leftImg8bit.png"):
+                    img_path = os.path.join(img_city_path, fname)
+                    lbl_name = fname.replace("_leftImg8bit", "_gtFine_labelTrainIds")
+                    lbl_path = os.path.join(lbl_city_path, lbl_name)
+                    self.samples.append((img_path, lbl_path))
 
-        for city in os.listdir(self.images_dir):
-            img_folder = os.path.join(self.images_dir, city)
-            lbl_folder = os.path.join(self.labels_dir, city)
-            for file_name in os.listdir(img_folder):
-                if file_name.endswith("_leftImg8bit.png"):
-                    img_path = os.path.join(img_folder, file_name)
-                    label_name = file_name.replace("_leftImg8bit", "_gtFine_labelIds")
-                    lbl_path = os.path.join(lbl_folder, label_name)
+    def __len__(self) -> int:
+        return len(self.samples)
 
-                    self.image_paths.append(img_path)
-                    self.label_paths.append(lbl_path)
+    def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor]:
+        img_path, lbl_path = self.samples[idx]
 
-    def __getitem__(self, idx):
-        image = Image.open(self.image_paths[idx]).convert("RGB")
-        label = Image.open(self.label_paths[idx])
-
-        image = np.array(image)
-        label = np.array(label)
+        image = np.array(Image.open(img_path).convert("RGB"))
+        label = np.array(Image.open(lbl_path).convert("L"))  # grayscale for labels
 
         if self.transform:
-            augmented = self.transform(image=image, mask=label)
-            image = augmented["image"]
-            label = augmented["mask"]
+            transformed = self.transform(image=image, mask=label)
+            image = transformed["image"]
+            label = transformed["mask"]
+
+        image = torch.from_numpy(image).permute(2, 0, 1).float() / 255.0
+        label = torch.from_numpy(label).long()
 
         return image, label
-
-    def __len__(self):
-        return len(self.image_paths)
