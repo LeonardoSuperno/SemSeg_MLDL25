5e9f4bdcbb3245213d9dc3f9d036e6f4d197fa96
giovannistina <giovanni.stina@gmail.com>
2025-04-24 12:21:26 +0200

    Update gta5.py

--- datasets/gta5.py
+++ datasets/gta5.py
@@ -1,22 +1,53 @@
 from torch.utils.data import Dataset
-
-# TODO: implement here your custom dataset class for GTA5
+import torch
+from PIL import Image
+import os
+import numpy as np
+from typing import Optional, Tuple
+from albumentations import Compose
 
 
 class GTA5(Dataset):
-    def __init__(self):
-        super(GTA5, self).__init__()
-        # TODO
+    """
+    Custom Dataset for the GTA5 dataset.
+    Loads synthetic images and their corresponding labelTrainIds masks.
+    """
+    def __init__(self, 
+                 root_dir: str, 
+                 transform: Optional[Compose] = None):
+        """
+        Args:
+            root_dir (str): Root directory of GTA5 dataset.
+            transform (Optional[Compose]): Albumentations transform to apply.
+        """
+        super().__init__()
+        self.transform = transform
+        self.samples = []
 
-        pass
+        img_dir = os.path.join(root_dir, "images")
+        lbl_dir = os.path.join(root_dir, "labels")
 
-    def __getitem__(self, idx):
-        # TODO
+        for fname in sorted(os.listdir(img_dir)):
+            if fname.endswith(".png"):
+                img_path = os.path.join(img_dir, fname)
+                lbl_path = os.path.join(lbl_dir, fname)  # stesso nome, due cartelle diverse
+                self.samples.append((img_path, lbl_path))
 
-        pass
+    def __len__(self) -> int:
+        return len(self.samples)
 
-    def __len__(self):
+    def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor]:
+        img_path, lbl_path = self.samples[idx]
 
-        # TODO
+        image = np.array(Image.open(img_path).convert("RGB"))
+        label = np.array(Image.open(lbl_path).convert("L"))  # label in grayscale (TrainIDs)
 
-        pass
+        if self.transform:
+            transformed = self.transform(image=image, mask=label)
+            image = transformed["image"]
+            label = transformed["mask"]
+
+        image = torch.from_numpy(image).permute(2, 0, 1).float() / 255.0
+        label = torch.from_numpy(label).long()
+
+        return image, label
